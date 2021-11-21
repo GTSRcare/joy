21594cc53dc5471d6996a6766d5a91692ae18d76
ram <ramthemarimuthu@gmail.com>
2021-11-20 21:13:14 -0500

    compliment controller and routes working

--- server/routes/complimentRoutes.js
+++ server/routes/complimentRoutes.js
@@ -8,13 +8,25 @@ router.get('/', complimentController.getCompliments, (req, res) => {
   return res
     .set('Content-Type', 'application/json')
     .status(200)
-    .json({ compliments: res.locals.compliments });
+    .json({ complimentsList: res.locals.complimentsList });
 });
 
 //POST request to /compliments?user=1
-// router.post('/', complimentController.getCompliments, (req, res) => {
-//   return res
-//     .set('Content-Type', 'application/json')
-//     .status(200)
-//     .json({ compliments: res.locals.compliments });
-// });
+//responds with compliment that was created
+router.post('/', complimentController.postCompliment, (req, res) => {
+  return res.json({ compliment: res.locals.compliment });
+});
+
+router.get('/tags', complimentController.getTags, (req, res) => {
+  return res.json({ tags: res.locals.tags });
+});
+
+router.patch('/', complimentController.updateCompliment, (req, res) => {
+  return res.json({ compliment: res.locals.compliment });
+});
+
+router.delete('/', complimentController.deleteCompliment, (req, res) => {
+  return res.status(200).send({ message: 'compliment deleted' });
+});
+
+module.exports = router;
