d74c6aa5ea35bbc1f1912f086897d875f0ae8bd8
gkokhabi <91689217+gkokhabi@users.noreply.github.com>
2021-11-22 00:15:30 -0500

    finish css for the sign up/login/viewall and redux

--- client/components/ComplimentsForm.jsx
+++ client/components/ComplimentsForm.jsx
@@ -6,12 +6,13 @@ import { postComplimentActionCreator } from '../actions/action';
 import NavBar from './NavBar.jsx';
 import '../stylesCCC.css';
 
-// Map state to props
+// Map state to props =>they are grabbing the userId that belongs to the compliement out of the state have it readily accessible to the props
 const mapStateToProps = state => ({
   user_id: state.compliments.user_id
 });
 
-// Map dispatch to props 
+
+// Map dispatch to props  =>invoking the dispatch and passing in the action creator to do the action we want 
 const mapDispatchToProps = dispatch => ({
   createCompliment: (user, category, sender, message) => dispatch(postComplimentActionCreator(user, category, sender, message))
 });
@@ -23,16 +24,22 @@ class ComplimentsForm extends Component {
   }
 
   // Handle form submit to create new compliment
-  handleSubmit() {
+  handleSubmit(e) {
+    e.preventDefault();
     const sender = document.getElementById('sender').value;
     const message = document.getElementById('message').value;
-    const category = document.getElementById('category').value;
+    const category = document.getElementById('category-select').value;
 
     // Dispatch action creator 
-    console.log('submitted')
+    console.log('submitted');
     // user, category, sender ,message 
     const user_id = this.props.user_id;
     this.props.createCompliment(user_id, category, sender, message);
+
+    // Clear form
+    document.getElementById('sender').value = '';
+    document.getElementById('message').value = '';
+    document.getElementById('category-select').value = 'Webpack'
   }
 
   render() {
@@ -44,31 +51,38 @@ class ComplimentsForm extends Component {
           <h2 className="text">Compliments Form</h2>
           
           <form className='ComplimentsForm'> 
-          <div className="senderDiv">
-            <label className="text" >Sender:  </label>
-            <input type='text' id='sender' />
+          <div className="senderAndMessageDiv">
+            <div className="senderDiv">
+              <label className="text" >Sender: </label>
+              <input type='text' id='sender' />
+            </div>
+
+            <div className="messageDiv">
+              <label>Message: <input type="text" id='message'></input></label>
+            </div>
           </div>
-          <div id="pick">
-            <label className="text">Pick a Category:</label>
-            <select id='category' name='category'>
-              <option value='Webpack'>Webpack</option>
-              <option value='General'>General</option>
-              <option value='Personality'>Personality</option>
-              <option value='Redux'>Redux</option>
-              <option value='webpack'>webpack</option>
-            </select>
-          </div>
-          <div>
-            <label className="text">Message:</label>
-            <input type='text' id='message' />
-          </div>
+
+          <div className="categoryPicking">
+            <div className="labelDropDown">
+              <label className="text">Category:</label>
+            </div>
+            
+            <div className="dropdownPicking">
+              <select id='category-select' name='category-select'>
+                <option value='Webpack'>Webpack</option>
+                <option value='General'>General</option>
+                <option value='Personality'>Personality</option>
+                <option value='Redux'>Redux</option>
+                <option value='webpack'>webpack</option>
+              </select>
+            </div>
+            </div>
+
+
           <button id='createComplimentButton' onClick={this.handleSubmit}>
             <div> Create a Compliment </div>
           </button>
-
           </form>
-         
-        
         </div >
       </div>
     );
@@ -77,3 +91,6 @@ class ComplimentsForm extends Component {
 // export default ComplimentsForm;
 export default connect(mapStateToProps, mapDispatchToProps)(ComplimentsForm);
 
+
+
+//
