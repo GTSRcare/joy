ac3ff054f512cd3df8384d6b958dea3c310bba09
gkokhabi <91689217+gkokhabi@users.noreply.github.com>
2021-11-21 21:04:53 -0500

    committing as if 11/21/2021

--- client/components/Dashmain.jsx
+++ client/components/Dashmain.jsx
@@ -8,7 +8,7 @@ class Dashmain extends Component {
         <div className='spacer'/>
         <div className='dash-container' > 
           <h3 className='general-compliment'>You are awesome! Keep it up!</h3>
-          <h3 className='select' >Select from dropdown</h3>
+          <h3 className='select' >Select from ADD DROPDOWN !!! dropdown</h3>
         </div>
         <div className='spacer2'/>
       </div>
